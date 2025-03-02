6b0ca0c19543f77e2d62f01ff9dea34accdde28e
Pedro Florit Docavo <pedroflorit@msn.com>
2025-03-02 17:14:48 +0000

    all_modals_done

--- src/api/models.py
+++ src/api/models.py
@@ -60,21 +60,25 @@ class Medias(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.Enum("done",  name="ok"), nullable=False)
     url = db.Column(db.String, unique=True, nullable=False)
-    
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    post_to = db.relationship("Posts", foreign_keys=[post_id], backref=db.backref("medias_to", lazy="select"))
 
 
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String, unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_to = db.relationship("Users", foreign_keys=[user_id], backref=db.backref("comments_to", lazy="select"))
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    post_to = db.relationship("Posts", foreign_keys=[post_id], backref=db.backref("comments_to", lazy="select"))
     
-
 
 class Followers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     following_id = db.Column(db.Integer, db.ForeignKey("users.id"))  # Columna de clave foranea 
     following_to = db.relationship("Users", foreign_keys=[following_id], backref=db.backref("following_to", lazy="select"))  
     follower_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    follower_to = db.relationship("Users", foreign_keys[follower_id], backref=db.backref("follower_to", lazy="select"))
+    follower_to = db.relationship("Users", foreign_keys=[follower_id], backref=db.backref("follower_to", lazy="select"))
 
 
 class Posts(db.Model):
@@ -84,6 +88,8 @@ class Posts(db.Model):
     date = db.Column(db.DateTime, default=datetime)
     body = db.Column(db.String, nullable=False)
     img_url = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_to = db.relationship("Users", foreign_keys=[user_id], backref=db.backref("posts_to", lazy="select"))
     
 
 class Characters(db.Model):
@@ -98,6 +104,14 @@ class Characters(db.Model):
     gender = db.Column(db.String, nullable=True)
     
 
+class PlanetFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_to = db.relationship("Users", foreign_keys=[user_id], backref=db.backref("planetfavorites_to", lazy="select"))
+    planet_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+    planet_to = db.relationship("Planets", foreign_keys=[planet_id], backref=db.backref("planets_to", lazy="select"))
+
+    
 class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=True)
@@ -112,11 +126,11 @@ class Planets(db.Model):
 
 class CharacterFavorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    
-    
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_to = db.relationship("Users", foreign_keys=[user_id], backref=db.backref("characterfavorites_to", lazy="select"))
+    character_id = db.Column(db.Integer, db.ForeignKey("characters.id"))
+    character_to = db.relationship("Characters", foreign_keys=[character_id], backref=db.backref("characters_to", lazy="select"))
 
 
-class PlanetFavorites(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
     
     
