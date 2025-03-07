ceb3ca00d87bc47b5f758c3bbe0396ab6adc7d4d
Pedro Florit Docavo <pedroflorit@msn.com>
2025-03-07 20:55:07 +0000

    methods

--- src/api/models.py
+++ src/api/models.py
@@ -24,46 +24,6 @@ class Users(db.Model):
                 "last_name": self.last_name}
     
 
-class Products(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    description = db.Column(db.String, unique=False, nullable=True)
-    price = db.Column(db.Float, nullable=False)
-
-    def __repr__(self):
-        return f"<Product: {self.name}>"
-
-    def serialize(self):
-        return {"id": self.id,
-                "name": self.name,
-                "description": self.description,
-                "price": self.price}
-    
-
-class Bills(db.Model):
-    __tablename__ = "bills"
-    id = db.Column(db.Integer, primary_key=True)
-    create_at = db.Column(db.DateTime, nullable=False, default=datetime)  # Default, el dia de creacion
-    total = db.Column(db.Float, nullable=False)
-    bill_address = db.Column(db.String)
-    status = db.Column(db.Enum("pending", "paid", "cancel", name="status"), nullable=False)
-    payment_method = db.Column(db.Enum("visa", "amex", "paypal", name="payment"), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    user_to = db.relationship("Users", foreign_keys=[user_id], backref=db.backref("bills_to", lazy="select"))
-
-
-class BillItems(db.Model):
-    __tablename__ = "bill_items"
-    id = db.Column(db.Integer, primary_key=True)
-    price_per_unit = db.Column(db.Float, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    total_price = db.Column(db.Float, nullable=False) 
-    bill_id = db.Column(db.Integer, db.ForeignKey("bills.id"))
-    bill_to = db.relationship("Bills", foreign_keys=[bill_id], backref=db.backref("bill_items", lazy="select"))
-    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
-    product_to = db.relationship("Products", foreign_keys=[product_id], backref=db.backref("bill_items", lazy="select"))
-   
-
 class Medias(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.Enum("done",  name="ok"), nullable=False)
@@ -102,43 +62,125 @@ class Posts(db.Model):
 
 class Characters(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=True)
-    height = db.Column(db.String, nullable=True)
-    mass = db.Column(db.String, nullable=True)
-    hair_color = db.Column(db.String, nullable=True)
-    skin_color = db.Column(db.String, nullable=True)
-    eye_color = db.Column(db.String, nullable=True)
-    birth_year = db.Column(db.String, nullable=True)
-    gender = db.Column(db.String, nullable=True)
+    name = db.Column(db.String)
+    height = db.Column(db.String)
+    mass = db.Column(db.String)
+    hair_color = db.Column(db.String)
+    skin_color = db.Column(db.String)
+    eye_color = db.Column(db.String)
+    birth_year = db.Column(db.String)
+    gender = db.Column(db.String)
     
+    def __repr__(self):
+        return f"<Characters: {self.id} - {self.name}>"
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "height": self.height,
+                "mass": self.mass,
+                "hair_color": self.hair_color,
+                "skin_color": self.skin_color,
+                "eye_color": self.eye_color,
+                "birth_year": self.birth_year,
+                "gender": self.gender}
+
+    
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    diameter = db.Column(db.String)
+    rotation_period = db.Column(db.String)
+    orbital_period = db.Column(db.String)
+    gravity = db.Column(db.String)
+    population = db.Column(db.String)
+    climate = db.Column(db.String)
+    terrain = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Character: {self.id} - {self.name}>'
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "climate": self.climate,
+                "surface_water": self.surface_water,
+                "diameter": self.diameter,
+                "rotation_period": self.rotation_period,
+                "terrain": self.terrain,
+                "gravity": self.gravity,
+                "orbital_period": self.orbital_period,
+                "population": self.population}
+
 
 class PlanetFavorites(db.Model):
+    __tablename__= "planet_favorites"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     user_to = db.relationship("Users", foreign_keys=[user_id], backref=db.backref("planetfavorites_to", lazy="select"))
     planet_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
     planet_to = db.relationship("Planets", foreign_keys=[planet_id], backref=db.backref("planets_to", lazy="select"))
 
-    
-class Planets(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=True)
-    diameter = db.Column(db.String, nullable=True)
-    rotation_period = db.Column(db.String, nullable=True)
-    orbital_period = db.Column(db.String, nullable=True)
-    gravity = db.Column(db.String, nullable=True)
-    population = db.Column(db.String, nullable=True)
-    climate = db.Column(db.String, nullable=True)
-    terrain = db.Column(db.String, nullable=True)
+    def __repr__(self):
+        return f"<PlanetFavorites: {self.id}>"
+    def serialize(self):
+         return {"id": self.id,
+                 "user_id": self.user_id,
+                 "planet_id": self.planet_id}
 
 
 class CharacterFavorites(db.Model):
+    __tablename__= "character_favorites"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     user_to = db.relationship("Users", foreign_keys=[user_id], backref=db.backref("characterfavorites_to", lazy="select"))
     character_id = db.Column(db.Integer, db.ForeignKey("characters.id"))
     character_to = db.relationship("Characters", foreign_keys=[character_id], backref=db.backref("characters_to", lazy="select"))
 
-
+    def __repr__(self):
+        return f"<CharacterFavorites: {self.id}>"
+    def serialize(self):
+         return {"id": self.id,
+                 "user_id": self.user_id,
+                 "character_id": self.character_id}
     
+    
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String, unique=False, nullable=True)
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"<Product: {self.name}>"
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "description": self.description,
+                "price": self.price}
+    
+
+class Bills(db.Model):
+    __tablename__ = "bills"
+    id = db.Column(db.Integer, primary_key=True)
+    create_at = db.Column(db.DateTime, nullable=False, default=datetime)  # Default, el dia de creacion
+    total = db.Column(db.Float, nullable=False)
+    bill_address = db.Column(db.String)
+    status = db.Column(db.Enum("pending", "paid", "cancel", name="status"), nullable=False)
+    payment_method = db.Column(db.Enum("visa", "amex", "paypal", name="payment"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_to = db.relationship("Users", foreign_keys=[user_id], backref=db.backref("bills_to", lazy="select"))
+
+
+class BillItems(db.Model):
+    __tablename__ = "bill_items"
+    id = db.Column(db.Integer, primary_key=True)
+    price_per_unit = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False) 
+    bill_id = db.Column(db.Integer, db.ForeignKey("bills.id"))
+    bill_to = db.relationship("Bills", foreign_keys=[bill_id], backref=db.backref("bill_items", lazy="select"))
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+    product_to = db.relationship("Products", foreign_keys=[product_id], backref=db.backref("bill_items", lazy="select"))    
     
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,18 +4,15 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users, Products
+from api.models import db, Users, CharacterFavorites, PlanetFavorites
+import requests
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 
 
 api = Blueprint('api', __name__)
 CORS(api) # Allow CORS requests to this API
-
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-    response_body = {}
-    response_body["message"] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    return response_body, 200
 
 
 @api.route("/users", methods=["GET"])
@@ -34,7 +31,178 @@ def users():
     return response_body, 200
 
 
-@api.route("/products", methods=["GET", "POST"])
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    response_body = {}
+    data = request.json
+    email = request.json.get("email", None)
+    password = data.get("password", None)
+    row = db.session.execute(db.select(Users).where(Users.email==email, Users.password==password, Users.is_active)).scalar()
+    # Si la consulta es exitosa, row tendrá algo (por lo tanto es verdadero), si no devuelve none
+    if not row:
+        response_body["message"] = "Bad username or password"
+        return response_body, 401
+
+    claims = {"user_id" : row[id]}    
+    access_token = create_access_token(identity=email)
+    response_body["message"] = "User logged!"
+    response_body["access_token"] = access_token
+    return response_body, 200
+
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    response_body = {}
+    current_user = get_jwt_identity()
+    response_body['message'] = f'User logged: {current_user}'
+    return response_body, 200
+
+
+@api.route('/characters', methods=['GET'])
+def characters():
+    response_body = {}
+    url = 'https://swapi.tech/api/people'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data['results'])
+        response_body['message'] = 'Listado de Personajes'
+        response_body['results'] = data['results']
+        return response_body, 200
+    response_body['message'] = 'algo salió mal'
+    return response_body, 400
+
+
+@api.route('/characters/<int:character_id>', methods=['GET'])
+def character(character_id):
+    response_body = {}
+    url = f'https://swapi.tech/api/people/{character_id}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data['result'])
+        response_body['message'] = 'Detalles del Personaje'
+        response_body['results'] = data['result']['properties']
+        return response_body, 200
+    response_body['message'] = 'algo salió mal'
+    return response_body, 400
+
+
+@api.route('/planets', methods=['GET'])
+def planets():
+    response_body = {}
+    url = 'https://swapi.tech/api/planets'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data['results'])
+        response_body['message'] = 'Listado de Planetas'
+        response_body['results'] = data['results']
+        return response_body, 200
+    response_body['message'] = 'algo salió mal'
+    return response_body, 400
+
+
+@api.route('/planets/<int:planet_id>', methods=['GET'])
+def planet(planet_id):
+    response_body = {}
+    url = f'https://swapi.tech/api/planets/{planet_id}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data['result'])
+        response_body['message'] = 'Detalles del Planeta'
+        response_body['results'] = data['result']['properties']
+        return response_body, 200
+    response_body['message'] = 'algo salió mal'
+    return response_body, 400
+
+
+@api.route('/users/<int:user_id>/favorites-planets', methods=['GET', 'POST'])
+def user_favorites_planets(user_id):
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(PlanetFavorites).where(PlanetFavorites.user_id == user_id)).scalars()
+        result_planets = [row.serialize() for row in rows]
+        response_body["message"] = f"Listado de Planetas Favoritos del usuario {user_id}"
+        response_body["results"] = result_planets
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json
+        row = PlanetFavorites(planet_id=data['planet_id'], user_id=user_id )
+        db.session.add(row)
+        db.session.commit()
+        response_body['message'] = f'Se guardo exitosamente'
+        return response_body, 200
+    
+
+@api.route('/users/<int:user_id>/favorites-characters', methods=['GET', 'POST'])
+def user_favorites_characters(user_id):
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(CharacterFavorites).where(CharacterFavorites.user_id == user_id)).scalars()
+        result_characters = [row.serialize() for row in rows]
+        response_body["message"] = f"Listado de Characters Favoritos del usuario {user_id}"
+        response_body["results"] = result_characters
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json
+        row = CharacterFavorites(character_id=data['character_id'], user_id=user_id )
+        db.session.add(row)
+        db.session.commit()
+        response_body['message'] = f'Se guardo exitosamente'
+        return response_body, 200
+
+
+@api.route("/users/<int:user_id>/favorite-characters/<int:character_id>", methods=['DELETE'])
+def delete_user_favorites_character(user_id, character_id):
+    response_body = {}
+    row = db.session.execute(db.select(CharacterFavorites).where(CharacterFavorites.user_id == user_id, CharacterFavorites.character_id == character_id)).scalar()
+    db.session.delete(row)
+    db.session.commit()
+    response_body["message"] = "Se borró con éxito"
+    return response_body, 200 
+
+
+@api.route("/users/<int:user_id>/favorite-planets/<int:planet_id>", methods=['DELETE'])
+def delete_user_favorites_planet(user_id, planet_id):
+    response_body = {}
+    row = db.session.execute(db.select(PlanetFavorites).where(PlanetFavorites.user_id == user_id, PlanetFavorites.planet_id == planet_id)).scalar()
+    db.session.delete(row)
+    db.session.commit()
+    response_body["message"] = "Se borró con éxito"
+    return response_body, 200 
+
+
+""" @api.route("/characters", methods=["GET"])
+def characters():
+    response_body = {}
+    rows = db.session.execute(db.select(Characters)).scalars()
+    # Opcion 2
+    # variable = [target for individual in iterable]
+    results = [row.serialize() for row in rows]
+    response_body["message"] = f"Listado de Characters"
+    response_body["results"] = results
+    return response_body, 200 """
+
+
+""" @api.route("/planets", methods=["GET"])
+def planets():
+    response_body = {}
+    rows = db.session.execute(db.select(Planets)).scalars()
+    # Opcion 2
+    # variable = [target for individual in iterable]
+    results = [row.serialize() for row in rows]
+    response_body["message"] = f"Listado de Planets"
+    response_body["results"] = results
+    return response_body, 200 """
+
+
+""" @api.route("/products", methods=["GET", "POST"])
 def products():
     response_body = {}
     if request.method == "GET":
@@ -53,10 +221,10 @@ def products():
         db.session.commit()
         response_body["message"] = f"Respuesta para el metodo {request.method}"
         response_body["results"] = row.serialize()
-        return response_body, 200
+        return response_body, 200 """
 
 
-@api.route("/products/<int:id>", methods=["GET", "PUT", "DELETE"])
+""" @api.route("/products/<int:id>", methods=["GET", "PUT", "DELETE"])
 def product(id):
     response_body = {}
     row = db.session.execute(db.select(Products).where(Products.id == id)).scalar()
@@ -86,6 +254,12 @@ def product(id):
         db.session.commit()
         response_body["message"] = f"Hemos borrado el producto id {id}"
         response_body["results"] = {}
-        return response_body, 200
+        return response_body, 200 """
 
 
+""" # Quiero obtener todos los estudiantes de la cohorte 93
+@api.route("/cohorts/<int:cohort_id>/students", methods=["GET"])
+def cohorts_students(cohort_id):
+    response_body = {}
+    # lógica para retornar esos datos
+    return response_body, 200 """
